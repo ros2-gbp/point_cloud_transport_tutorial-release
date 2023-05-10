6c6f128aa4a12eb8e7783587c623640fcd1cae37
Martin Pecka <peckama2@fel.cvut.cz>
2023-05-10 19:03:51 +0200

    Initial cleanup before releasing. No substantial changes made.

--- src/my_publisher.cpp
+++ src/my_publisher.cpp
@@ -1,33 +1,35 @@
-#include <ros/ros.h>
-#include <sensor_msgs/PointCloud2.h>
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-FileCopyrightText: Czech Technical University in Prague .. 2019, paplhjak
+
 #include <point_cloud_transport/point_cloud_transport.h>
+#include <ros/ros.h>
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
+#include <sensor_msgs/PointCloud2.h>
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "point_cloud_publisher");
-    ros::NodeHandle nh;
+  ros::init(argc, argv, "point_cloud_publisher");
+  ros::NodeHandle nh;
 
-    point_cloud_transport::PointCloudTransport pct(nh);
-    point_cloud_transport::Publisher pub = pct.advertise("pct/point_cloud", 100);
+  point_cloud_transport::PointCloudTransport pct(nh);
+  point_cloud_transport::Publisher pub = pct.advertise("pct/point_cloud", 100);
 
-    rosbag::Bag bag;
-    bag.open(argv[1], rosbag::bagmode::Read);
+  rosbag::Bag bag;
+  bag.open(argv[1], rosbag::bagmode::Read);
 
-    ros::Rate loop_rate(5);
-    for(rosbag::MessageInstance const m: rosbag::View(bag))
+  ros::Rate loop_rate(5);
+  for (const auto& m: rosbag::View(bag))
+  {
+    sensor_msgs::PointCloud2::ConstPtr i = m.instantiate<sensor_msgs::PointCloud2>();
+    if (i != nullptr)
     {
-        sensor_msgs::PointCloud2::ConstPtr i = m.instantiate<sensor_msgs::PointCloud2>();
-        if (i != nullptr)
-        {
-            pub.publish(i);
-            ros::spinOnce();
-            loop_rate.sleep();
-        }
-        if (!nh.ok())
-        {
-         break;
-        }
+      pub.publish(i);
+      ros::spinOnce();
+      loop_rate.sleep();
     }
+
+    if (!ros::ok())
+      break;
+  }
 }
--- src/my_subscriber.cpp
+++ src/my_subscriber.cpp
@@ -1,21 +1,24 @@
-#include <ros/ros.h>
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-FileCopyrightText: Czech Technical University in Prague .. 2019, paplhjak
+
 #include <point_cloud_transport/point_cloud_transport.h>
+#include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 
 void Callback(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
-    std::cout << "Message received, number of points is: " << msg->width*msg->height << std::endl;
+  std::cout << "Message received, number of points is: " << msg->width * msg->height << std::endl;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "point_cloud_subscriber");
-    ros::NodeHandle nh;
+  ros::init(argc, argv, "point_cloud_subscriber");
+  ros::NodeHandle nh;
 
-    point_cloud_transport::PointCloudTransport pct(nh);
-    point_cloud_transport::Subscriber sub = pct.subscribe("pct/point_cloud", 100, Callback);
-    ros::spin();
+  point_cloud_transport::PointCloudTransport pct(nh);
+  point_cloud_transport::Subscriber sub = pct.subscribe("pct/point_cloud", 100, Callback);
+  ros::spin();
 
-    return 0;
+  return 0;
 }
 
